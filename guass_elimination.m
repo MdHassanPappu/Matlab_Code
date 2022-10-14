0065dbf72990c06d570a8583b1864ff07554a438
MdHassanPappu <99087593+MdHassanPappu@users.noreply.github.com>
2022-10-14 21:18:22 +0600

    Add files via upload

--- guass_elimination.m
+++ guass_elimination.m
@@ -1,7 +1,9 @@
-M=[6 2 -1;2 1 -2; -3 1 1]
+M=[6 2 -1;2 1 -2; -3 1 189]
 b=[3 ;3;-6]
 A=[M b]
 n=3
+s=rref(A)
+s=s(:,end)
 for j=1:n-1
     for i=j+1:n
         m=A(i,j)/A(j,j)
@@ -9,13 +11,12 @@ for j=1:n-1
     end 
 end 
 
-Aug=A
 b=A(:,end)
 x(n)=b(n)/A(n,n)
+
 for i=n-1:-1:1
-    sum=b(i);
-     
-    for j=i+1:n
+    sum=b(i); 
+     for j=i+1:n
         sum=sum-A(i,j)*x(j)
     end 
     x(i)=sum/A(i,i)
